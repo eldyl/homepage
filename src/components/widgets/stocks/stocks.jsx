47f6b6e02e58d1f36d496705f340e2dc94b698f1
Dylan Ullrich <dylan@eldyl.org>
2024-06-17 00:21:46 -0700

    fix: max 6 items in watchlist, error on duplicates

--- src/components/widgets/stocks/stocks.jsx
+++ src/components/widgets/stocks/stocks.jsx
@@ -6,7 +6,6 @@ import { FaChartLine } from "react-icons/fa6";
 import Error from "../widget/error";
 import Container from "../widget/container";
 import PrimaryText from "../widget/primary_text";
-import SecondaryText from "../widget/secondary_text";
 import WidgetIcon from "../widget/widget_icon";
 import Raw from "../widget/raw";
 
@@ -14,6 +13,10 @@ function Widget({ options }) {
   const { t, i18n } = useTranslation();
 
   const [viewingPercentChange, setViewingPercentChange] = useState(false);
+
+  const { useColor } = options;
+
+  console.log("use color", useColor);
 
   const { data, error } = useSWR(
     `/api/widgets/stocks?${new URLSearchParams({ lang: i18n.language, ...options }).toString()}`,
@@ -34,12 +37,31 @@ function Widget({ options }) {
 
   if (data) {
     const stocks = data.stocks.map((stock) => (
-      <span key={stock.ticker} className="flex flex-col items-center justify-center w-5 divide-y dark:divide-white/20">
-        <span className="text-theme-800 dark:text-theme-200 text-sm font-medium">{stock.ticker}</span>
+      <span
+        key={stock.ticker}
+        className="bg-theme-200/50 dark:bg-theme-900/20 rounded h-full ml-1 text-xs px-1.5 w-16 flex flex-col items-center justify-center "
+      >
+        <span className="text-theme-800 dark:text-theme-200 text-xs">{stock.ticker}</span>
         {!viewingPercentChange ? (
-          <SecondaryText>{stock.currentPrice}</SecondaryText>
+          <span
+            className={
+              useColor
+                ? `text-xs ${parseInt(data?.dp) < 0 ? "text-emerald-300" : "text-rose-300"}`
+                : "text-theme-800/70 dark:text-theme-200/50 text-xs"
+            }
+          >
+            {stock.currentPrice}
+          </span>
         ) : (
-          <SecondaryText>{stock.percentChange}%</SecondaryText>
+          <span
+            className={
+              useColor
+                ? `text-xs ${data?.dp < 0 ? "text-emerald-300" : "text-rose-300"}`
+                : "text-theme-800/70 dark:text-theme-200/70 text-xs"
+            }
+          >
+            {stock.percentChange}%
+          </span>
         )}
       </span>
     ));
@@ -49,10 +71,10 @@ function Widget({ options }) {
           <button
             type="button"
             onClick={() => (viewingPercentChange ? setViewingPercentChange(false) : setViewingPercentChange(true))}
-            className="flex items-center justify-center w-full h-full hover:outline-none focus:outline-none"
+            className="flex items-center w-full h-full hover:outline-none focus:outline-none"
           >
-            <FaChartLine className="information-widget-icon flex-none mr-3 w-5 h-5 text-theme-800 dark:text-theme-200" />
-            <div className="flex flex-wrap items-center pl-1 gap-x-7 mr-3">{stocks}</div>
+            <FaChartLine className="flex-none w-5 h-5 text-theme-800 dark:text-theme-200" />
+            <div className="flex flex-wrap items-center mx-2 gap-y-1">{stocks}</div>
           </button>
         </Raw>
       </Container>
