11d52d590fee1ea7698b745a240e2cb3ba14631e
Dylan Ullrich <dylan@eldyl.org>
2024-10-26 16:00:00 -0700

    Reduce data returned by API endpoint to avoid 4mb limit

--- src/widgets/radarr/widget.js
+++ src/widgets/radarr/widget.js
@@ -12,7 +12,10 @@ const widget = {
         wanted: jsonArrayFilter(data, (item) => item.monitored && !item.hasFile && item.isAvailable).length,
         have: jsonArrayFilter(data, (item) => item.hasFile).length,
         missing: jsonArrayFilter(data, (item) => item.monitored && !item.hasFile).length,
-        all: asJson(data),
+        all: asJson(data).map((entry) => ({
+          id: entry.id,
+          title: entry.title,
+        })),
       }),
     },
     "queue/status": {
